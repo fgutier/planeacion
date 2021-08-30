1775cbd7821aa9fd5e1b175dbdebf465dcbd50a0
Francisco Gutierrez <fockus.gutierrez@gmail.com>
2021-08-30 15:35:43 -0500

    estructura
    estructura de carpetas de como sera el proyecto

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modelo Planeacion MSFT - Foxconn</title>
+    <link rel="stylesheet" href="views/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="msft-pcba">
+            <h1>PCBA</h1>
+        </div>
+        <div class="msft-mech">
+            <h1>MECH</h1>
+        </div>
+    </div>
+</body>
+</html>
